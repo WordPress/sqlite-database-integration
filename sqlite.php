11495f5ea576fd98d5383f3c005a23039d4ce993
Ari Stathopoulos <aristath@gmail.com>
2022-09-22 13:28:05 +0300

    Add notice if the db.php file was not located

--- sqlite.php
+++ sqlite.php
@@ -5,6 +5,7 @@
  * Author: Ari Stathopoulos
  * Version: 1.0.0
  * Requires PHP: 5.6
+ * Textdomain: sqlite
  *
  * This project is based on the original work of Kojima Toshiyasu and his SQLite Integration plugin,
  * and the work of Evan Mattson and his WP SQLite DB plugin - See https://github.com/aaemnnosttv/wp-sqlite-db
@@ -49,3 +50,12 @@ register_activation_hook( __FILE__, 'sqlite_plugin_copy_db_file' );
 
 // Remove db.php file on plugin deactivation.
 register_deactivation_hook( __FILE__, 'sqlite_plugin_remove_db_file' );
+
+
+add_action( 'admin_notices', function() {
+	if ( ! file_exists( WP_CONTENT_DIR . '/db.php' ) ) {
+		echo '<div class="notice notice-error">';
+		echo '<p>' . __( 'The wp-content/db.php file could not be detected. The SQLite integration will not work until that file is successfully copied. To try again automatically, deactivate the SQLite plugin and activate it again.', 'sqlite' ) . '</p>';
+		echo '</div>';
+	}
+} );
