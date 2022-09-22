2f5837fbcbd0828243cfee154e38970f914e8949
Ari Stathopoulos <aristath@gmail.com>
2022-09-22 14:12:21 +0300

    Add debug info in the health screen

--- sqlite.php
+++ sqlite.php
@@ -51,7 +51,6 @@ register_deactivation_hook( __FILE__, 'sqlite_plugin_remove_db_file' ); // Remov
  * Add admin notices.
  */
 function sqlite_plugin_admin_notice() {
-
 	// Check if the wp-content/db.php file exists.
 	if ( ! file_exists( WP_CONTENT_DIR . '/db.php' ) ) {
 		printf(
@@ -73,3 +72,56 @@ function sqlite_plugin_admin_notice() {
 	}
 }
 add_action( 'admin_notices', 'sqlite_plugin_admin_notice' ); // Add the admin notices.
+
+/**
+ * Filter debug data in site-health screen.
+ *
+ * @param array $info The debug data.
+ */
+function sqlite_plugin_filter_debug_data( $info ) {
+	$database_type = defined( 'DATABASE_TYPE' ) && 'sqlite' === DATABASE_TYPE ? 'sqlite' : 'mysql';
+	if ( 'sqlite' !== $database_type ) {
+		return $info;
+	}
+
+	$info['wp-constants']['fields']['DATABASE_TYPE'] = array(
+		'label' => 'DATABASE_TYPE',
+		'value' => ( defined( 'DATABASE_TYPE' ) ? DATABASE_TYPE : __( 'Undefined', 'sqlite' ) ),
+		'debug' => ( defined( 'DATABASE_TYPE' ) ? DATABASE_TYPE : 'undefined' ),
+	);
+
+	$info['wp-database']['fields']['database_type'] = array(
+		'label' => __( 'Database type' ),
+		'value' => 'sqlite' === $database_type ? 'SQLite' : 'MySQL/MariaDB',
+	);
+
+	$info['wp-database']['fields']['database_version'] = array(
+		'label' => __( 'SQLite version' ),
+		'value' => class_exists( 'SQLite3' ) ? SQLite3::version()['versionString'] : null,
+	);
+
+	$info['wp-database']['fields']['database_file'] = array(
+		'label'   => __( 'Database file' ),
+		'value'   => FQDB,
+		'private' => true,
+	);
+
+	$info['wp-database']['fields']['database_size'] = array(
+		'label' => __( 'Database size' ),
+		'value' => size_format( filesize( FQDB ) ),
+	);
+
+	unset( $info['wp-database']['fields']['extension'] );
+	unset( $info['wp-database']['fields']['server_version'] );
+	unset( $info['wp-database']['fields']['client_version'] );
+	unset( $info['wp-database']['fields']['database_host'] );
+	unset( $info['wp-database']['fields']['database_user'] );
+	unset( $info['wp-database']['fields']['database_name'] );
+	unset( $info['wp-database']['fields']['database_charset'] );
+	unset( $info['wp-database']['fields']['database_collate'] );
+	unset( $info['wp-database']['fields']['max_allowed_packet'] );
+	unset( $info['wp-database']['fields']['max_connections'] );
+
+	return $info;
+}
+add_filter( 'debug_information', 'sqlite_plugin_filter_debug_data' ); // Filter debug data in site-health screen.
