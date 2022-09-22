7cf7d35443dc0351efad0a3cba571fd01cbf4f2f
Ari Stathopoulos <aristath@gmail.com>
2022-09-22 13:20:06 +0300

    add deactivation hook

--- sqlite.php
+++ sqlite.php
@@ -10,19 +10,42 @@
  * and the work of Evan Mattson and his WP SQLite DB plugin - See https://github.com/aaemnnosttv/wp-sqlite-db
  */
 
-// Add the db.php file in wp-content.
-register_activation_hook( __FILE__, function() {
-	$db_file_path = WP_CONTENT_DIR . '/db.php';
-	if ( ! file_exists( $db_file_path ) ) {
+/**
+ * Add the db.php file in wp-content.
+ */
+function sqlite_plugin_copy_db_file() {
+	$destination = WP_CONTENT_DIR . '/db.php';
+	if ( ! file_exists( $destination ) ) {
 		global $wp_filesystem;
 		if ( ! $wp_filesystem ) {
 			require_once ABSPATH . '/wp-admin/includes/file.php';
 			WP_Filesystem();
 		}
-		if ( $wp_filesystem->touch( $db_file_path ) ) {
+		if ( $wp_filesystem->touch( $destination ) ) {
 			$file_contents = file_get_contents( __DIR__ . '/db.copy' );
 			$file_contents = str_replace( 'DB_PLUGIN_DIR', __DIR__, $file_contents );
-			$wp_filesystem->put_contents( $db_file_path, $file_contents );
+			$wp_filesystem->put_contents( $destination, $file_contents );
 		}
 	}
-} );
+}
+
+/**
+ * Delete the db.php file in wp-content.
+ */
+function sqlite_plugin_remove_db_file() {
+	$destination = WP_CONTENT_DIR . '/db.php';
+	if ( file_exists( $destination ) ) {
+		global $wp_filesystem;
+		if ( ! $wp_filesystem ) {
+			require_once ABSPATH . '/wp-admin/includes/file.php';
+			WP_Filesystem();
+		}
+		$wp_filesystem->delete( $destination );
+	}
+}
+
+// Copy db.php file on plugin activation.
+register_activation_hook( __FILE__, 'sqlite_plugin_copy_db_file' );
+
+// Remove db.php file on plugin deactivation.
+register_deactivation_hook( __FILE__, 'sqlite_plugin_remove_db_file' );
