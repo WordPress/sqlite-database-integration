d5d86e0ed36f2619b4afd2094f81f5e3a245678f
Ari Stathopoulos <aristath@gmail.com>
2022-09-22 13:11:40 +0300

    Add db.php file in wp-content

--- sqlite.php
+++ sqlite.php
@@ -10,4 +10,19 @@
  * and the work of Evan Mattson and his WP SQLite DB plugin - See https://github.com/aaemnnosttv/wp-sqlite-db
  */
 
-require_once __DIR__ . '/sqlite.php';
+// Add the db.php file in wp-content.
+register_activation_hook( __FILE__, function() {
+	$db_file_path = WP_CONTENT_DIR . '/db.php';
+	if ( ! file_exists( $db_file_path ) ) {
+		global $wp_filesystem;
+		if ( ! $wp_filesystem ) {
+			require_once ABSPATH . '/wp-admin/includes/file.php';
+			WP_Filesystem();
+		}
+		if ( $wp_filesystem->touch( $db_file_path ) ) {
+			$file_contents = file_get_contents( __DIR__ . '/db.copy' );
+			$file_contents = str_replace( 'DB_PLUGIN_DIR', __DIR__, $file_contents );
+			$wp_filesystem->put_contents( $db_file_path, $file_contents );
+		}
+	}
+} );
