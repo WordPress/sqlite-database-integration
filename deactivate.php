fa030bece4f95273656cfdd693f46cead2c5ea65
Ari Stathopoulos <aristath@gmail.com>
2023-01-10 11:07:57 +0200

    deactivate plugin in mysql db from sqlite

--- deactivate.php
+++ deactivate.php
@@ -24,5 +24,35 @@ function sqlite_plugin_remove_db_file() {
 	if ( $wp_filesystem || WP_Filesystem() ) {
 		$wp_filesystem->delete( WP_CONTENT_DIR . '/db.php' );
 	}
+
+	// Run an action on `shutdown`, to deactivate the option in the MySQL database.
+	add_action(
+		'shutdown',
+		function() {
+			global $table_prefix;
+
+			// Get credentials for the MySQL database.
+			$dbuser     = defined( 'DB_USER' ) ? DB_USER : '';
+			$dbpassword = defined( 'DB_PASSWORD' ) ? DB_PASSWORD : '';
+			$dbname     = defined( 'DB_NAME' ) ? DB_NAME : '';
+			$dbhost     = defined( 'DB_HOST' ) ? DB_HOST : '';
+
+			// Init a connection to the MySQL database.
+			$wpdb_mysql = new wpdb( $dbuser, $dbpassword, $dbname, $dbhost );
+			$wpdb_mysql->set_prefix( $table_prefix );
+
+			// Get the perflab options, remove the database/sqlite module and update the option.
+			$row = $wpdb_mysql->get_row( $wpdb_mysql->prepare( "SELECT option_value FROM $wpdb_mysql->options WHERE option_name = %s LIMIT 1", 'active_plugins' ) );
+			if ( is_object( $row ) ) {
+				$value = maybe_unserialize( $row->option_value );
+				if ( is_array( $value ) ) {
+					$value_flipped = array_flip( $value );
+					unset( $value_flipped[ str_replace( WP_PLUGIN_DIR . '/', '', SQLITE_MAIN_FILE ) ] );
+					$value = array_flip( $value_flipped );
+					$wpdb_mysql->update( $wpdb_mysql->options, array( 'option_value' => maybe_serialize( $value ) ), array( 'option_name' => 'active_plugins' ) );
+				}
+			}
+		}
+	);
 }
 register_deactivation_hook( SQLITE_MAIN_FILE, 'sqlite_plugin_remove_db_file' ); // Remove db.php file on plugin deactivation.
