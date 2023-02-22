7e1af11f4fba00f9ca38b5d5d4f5e34e5647eae1
Ari Stathopoulos <aristath@gmail.com>
2023-02-22 14:31:14 +0200

    Up the version & update the readme file

--- load.php
+++ load.php
@@ -3,12 +3,14 @@
  * Plugin Name: SQLite Database Integration
  * Description: SQLite database driver drop-in. (based on SQLite Integration by Kojima Toshiyasu)
  * Author: WordPress Performance Team
- * Version: 1.0.3
+ * Version: 2.0
  * Requires PHP: 5.6
  * Textdomain: sqlite-database-integration
  *
  * This project is based on the original work of Kojima Toshiyasu and his SQLite Integration plugin,
  * and the work of Evan Mattson and his WP SQLite DB plugin - See https://github.com/aaemnnosttv/wp-sqlite-db
+ *
+ * @package wp-sqlite-integration
  */
 
 define( 'SQLITE_MAIN_FILE', __FILE__ );
