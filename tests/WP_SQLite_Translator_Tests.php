4e74594814bf28a54d47c0c3fb2ade1448a32f0b
Brandon Payton <brandon@happycode.net>
2024-04-29 20:54:33 -0400

    Restore necessary void returns 😬
    Revert "Remove unnecessary returns"
    
    This reverts commit b9c9faa67b7940aaecb45073167ea76f52647754.

--- tests/WP_SQLite_Translator_Tests.php
+++ tests/WP_SQLite_Translator_Tests.php
@@ -24,6 +24,7 @@ class WP_SQLite_Translator_Tests extends TestCase {
 			$GLOBALS['wpdb']->suppress_errors = false;
 			$GLOBALS['wpdb']->show_errors     = true;
 		}
+		return;
 	}
 
 	// Before each test, we create a new database
