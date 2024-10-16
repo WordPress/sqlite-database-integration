1765e4fdfa5c289721877fff44caf7b9360e18bc
Jan Jakes <jan@jakes.pro>
2024-10-16 10:58:46 +0200

    Add a script to parse grammar directly from MySQLParser.g4
    This simplifies the workflow, removes the need for additional conversion tools, and captures version specifiers in the grammar. We can also merge phpize-grammar into this script in the future.

--- custom-parser/grammar-factoring/create_grammar.sh
+++ custom-parser/grammar-factoring/create_grammar.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-bun 1-ebnf-to-json.js ./MySQLParser-manually-factored.ebnf > MySQLParser-manually-factored.json
-python 2-cli.py expand ./MySQLParser-manually-factored.json --format=json > ./MySQLParser-factored.json
-php 3-phpize-grammar.php ./MySQLParser-factored.json > ../parser/grammar.php
+php convert-grammar.php > MySQLParser-factored-versioned.json
+php 3-phpize-grammar.php ./MySQLParser-factored-versioned.json > ../parser/grammar.php
