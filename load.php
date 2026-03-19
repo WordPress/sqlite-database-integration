02e4b4fe47a36181144825956ef9af943c1aae5f
Jan Jakeš <jan@jakes.pro>
2026-03-19 15:26:55 +0100

    Update version to v2.2.19

--- load.php
+++ load.php
@@ -3,7 +3,7 @@
  * Plugin Name: SQLite Database Integration
  * Description: SQLite database driver drop-in.
  * Author: The WordPress Team
- * Version: 2.2.18
+ * Version: 2.2.19
  * Requires PHP: 7.2
  * Textdomain: sqlite-database-integration
  *
