791a7da7ec645a4f86bd9c594694801f216e1096
Chris Huber <chubes@extrachill.com>
2026-08-23 22:03:39 +0000

    test: avoid custom savepoint fixture table

--- tests/phpunit/WP_SQLite_Database_Integration_Savepoint_Test.php
+++ tests/phpunit/WP_SQLite_Database_Integration_Savepoint_Test.php
@@ -5,20 +5,20 @@ class WP_SQLite_Database_Integration_Savepoint_Test extends WP_UnitTestCase {
 	public function test_wpdb_update_inside_savepoint_does_not_open_nested_transaction() {
 		global $wpdb;
 
-		$table_name = $wpdb->prefix . 'sqlite_savepoint_write_test';
-		$wpdb->query( $wpdb->prepare( 'CREATE TABLE %i (id INT PRIMARY KEY, value INT)', $table_name ) );
+		$option_name = 'sqlite_savepoint_write_test';
 		$wpdb->insert(
-			$table_name,
+			$wpdb->options,
 			array(
-				'id'    => 1,
-				'value' => 1,
+				'option_name'  => $option_name,
+				'option_value' => '1',
+				'autoload'     => 'no',
 			)
 		);
 
 		try {
 			$this->assertNotFalse( $wpdb->query( 'SAVEPOINT wpdb_update' ) );
 
-			$result  = $wpdb->update( $table_name, array( 'value' => 2 ), array( 'id' => 1 ) );
+			$result  = $wpdb->update( $wpdb->options, array( 'option_value' => '2' ), array( 'option_name' => $option_name ) );
 			$queries = array_column( $wpdb->get_driver()->get_last_sqlite_queries(), 'sql' );
 
 			$this->assertSame( 1, $result );
@@ -26,12 +26,17 @@ class WP_SQLite_Database_Integration_Savepoint_Test extends WP_UnitTestCase {
 			$this->assertSame( '', $wpdb->last_error );
 			$this->assertNotContains( 'BEGIN IMMEDIATE', $queries );
 			$this->assertNotFalse( $wpdb->query( 'RELEASE SAVEPOINT wpdb_update' ) );
-			$this->assertSame( '2', $wpdb->get_var( $wpdb->prepare( 'SELECT value FROM %i WHERE id = 1', $table_name ) ) );
+			$this->assertSame(
+				'2',
+				$wpdb->get_var(
+					$wpdb->prepare( 'SELECT option_value FROM %i WHERE option_name = %s', $wpdb->options, $option_name )
+				)
+			);
 		} finally {
 			if ( $wpdb->get_driver()->inTransaction() ) {
 				$wpdb->query( 'ROLLBACK' );
 			}
-			$wpdb->query( $wpdb->prepare( 'DROP TABLE IF EXISTS %i', $table_name ) );
+			$wpdb->delete( $wpdb->options, array( 'option_name' => $option_name ) );
 		}
 	}
 }
